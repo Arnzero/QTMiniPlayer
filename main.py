53794494235cd0af0ecc93e397bf2fea443cd8fd
arnzero <afh146@humboldt.edu>
2020-04-10 22:47:06 -0700

    please work

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import sys
+import sys, os
 from PySide2.QtUiTools import QUiLoader #allows us to import .ui files
-from PySide2.QtWidgets import QApplication, QLineEdit, QPushButton, QFileDialog, QAction
+from PySide2.QtWidgets import QApplication, QLineEdit, QPushButton, QFileDialog, QAction, QSlider, QMouseEventTransition, QLabel
 from PySide2.QtCore import QFile, QObject, QUrl
 from PySide2.QtMultimedia import QMediaPlayer
+from PySide2.QtGui import QPixmap
 
 class MainWindow(QObject):
 
@@ -11,10 +12,22 @@ class MainWindow(QObject):
 
         #reference to our music player
         self.music_player = QMediaPlayer()
-        self.music_player.setVolume(100)
-
+        self.music_player.setVolume(89)
+        
         #call parent QObject constructor
         super(MainWindow, self).__init__(parent)
+
+        #add background image
+        """
+        self.title = "Adrian's boombox"
+        self.MainWindow.setWindowTitle(self.title)
+
+        label = QLabel(self)
+        pixmap = QPixmap('MW_background.jfif')
+        label.setPixmap(pixmap)
+        self.setCentralWidget(label)
+        self.resize(pixmap.width(), pixmap.height())
+        """
 
         #load the UI file into Python
         ui_file = QFile(ui_file)
@@ -35,18 +48,65 @@ class MainWindow(QObject):
         play_button = self.window.findChild(QPushButton, 'play_button')
         play_button.clicked.connect(self.play_button_clicked)
 
+        stop_button = self.window.findChild(QPushButton, 'stop_button')
+        stop_button.clicked.connect(self.stop_button_clicked)
+
+        volume_slider = self.window.findChild(QSlider, 'volume_slider')
+        """
+        #volume_slider.setFocusPolicy(QSlider.StrongFocus)
+        #volume_slider.clicked.connect(self.music_player.setVolume(15))
+        volume_slider.SliderToMaximum
+        """
+        volume_slider.setTickInterval(10)
+        volume_slider.setSingleStep(1)
+        volume_slider.setTickPosition(QSlider.TicksBothSides)
+        volume_slider.setMinimum(1)
+        volume_slider.setMaximum(100)
+        volume_slider.setValue(25)
+
+        volume_slider.valueChanged.connect(self.volume_change)
+
+
+        volume_plus = self.window.findChild(QPushButton, 'volume_plus')
+        volume_plus.clicked.connect(self.volume_plus_button_clicked)
+        
+        volume_minus = self.window.findChild(QPushButton, 'volume_minus')
+        volume_minus.clicked.connect(self.volume_minus_button_clicked)
+
+
         #show window to user
         self.window.show()
 
     def open_action_triggered(self):
         file_name = QFileDialog.getOpenFileName(self.window)
         self.music_player.setMedia(QUrl.fromLocalFile(file_name[0]))
+        song_label = self.window.findChild(QLabel, 'song_playing_label')
+        display_title = os.path.basename(str(file_name[0]))
+        song_label.setText(display_title)
+        
 
     def quit_action_triggered(self):
         self.window.close()
 
     def play_button_clicked(self):
         self.music_player.play()
+        
+
+    def stop_button_clicked(self):
+        self.music_player.stop()
+    
+    def volume_plus_button_clicked(self):
+        self.music_player.setVolume(100)
+
+    def volume_minus_button_clicked(self):
+        self.music_player.setVolume(0)
+
+    def volume_change(self):
+  
+        print(self.window.volume_slider.value()) # this prints volume of Qslider object
+        #print(self.music_player.volume()) # this prints volume of music_player object
+        self.music_player.setVolume(self.window.volume_slider.value())
+    
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
